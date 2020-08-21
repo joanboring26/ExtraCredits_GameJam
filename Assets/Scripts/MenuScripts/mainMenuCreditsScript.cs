a97d29f046c519b2b32ed15b4852e1eeee6760bb
Jake Smith <jakeconlonsmith@gmail.com>
2020-08-21 16:55:32 -0500

    main menu initial setup

---
+++ Assets/Scripts/MenuScripts/mainMenuCreditsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class mainMenuCreditsScript : MonoBehaviour
+{
+    [SerializeField] Button mainMenuButton;
+    GameObject [] CreditsObjects;
+    [SerializeField] mainMenuScript mainMenu;
+    void Awake()
+    {
+        mainMenuButton.onClick.AddListener(OnClickMainMenu);
+        CreditsObjects = GameObject.FindGameObjectsWithTag("MainMenuCreditsOnly");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMenu(false);
+    }
+
+
+    void OnClickMainMenu()
+    {
+        SetMenu(false);
+        mainMenu.SetMenu(true);
+    }
+
+    public void SetMenu(bool set)
+    {
+        foreach (GameObject g in CreditsObjects)
+            g.SetActive(set);
+    }
+}
---
+++ Assets/Scripts/MenuScripts/mainMenuScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class mainMenuScript : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+    [SerializeField] Button creditsButton;
+    [SerializeField] Button quitButton;
+
+    GameObject [] mainMenuObjects;
+    [SerializeField] mainMenuCreditsScript credits;
+    void Awake()
+    {
+        playButton.onClick.AddListener(OnClickPlay);
+        creditsButton.onClick.AddListener(OnClickCredits);
+        quitButton.onClick.AddListener(OnClickQuit);
+        mainMenuObjects = GameObject.FindGameObjectsWithTag("MainMenuOnly");
+    }
+
+    void OnClickPlay()
+    {
+        //start the scene
+    }
+
+    void OnClickCredits()
+    {
+        SetMenu(false);
+        credits.SetMenu(true);
+    }
+
+    void OnClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void SetMenu(bool set)
+    {
+        foreach(GameObject g in mainMenuObjects)
+            g.SetActive(set);
+    }
+}
