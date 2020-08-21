48f31bbcc113a4e65e4f60e966bd281cc3af17a1
Joan Pato <joan.pato@enti.cat>
2020-08-22 01:10:02 +0200

    Entity base done

---
+++ Assets/Scripts/entities/playerInputs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInputs : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+    }
+}
