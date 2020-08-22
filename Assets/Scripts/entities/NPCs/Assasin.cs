b33c66e939401029cee0f843ea97763f42b53d16
Joan Pato <joan.pato@enti.cat>
2020-08-22 20:22:35 +0200

    Started assasin script

---
+++ Assets/Scripts/entities/NPCs/Assasin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assasin : Character
+{
+    public Transform kingTransform;
+
+    private void Start()
+    {
+        //kingTransform = FindObjectOfType<kingAIScript>();
+    }
+
+    public override void CharacterUpdate()
+    {
+        float distX = transform.position.x - kingTransform.position.x;
+        float distZ = transform.position.z - kingTransform.position.z;
+        Vector3 dir = new Vector3(0, 0, 0);
+        if(distX > distZ)
+        {
+            if(distX > 0)
+            {
+                dir.x = 1;
+            }
+            else
+            {
+                dir.x = 1;
+            }
+        }
+    }
+
+}
