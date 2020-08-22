06744882b698e701d879fd4ae15555ae814ab34e
Josh <joshdev926>
2020-08-22 14:06:30 -0700

    Added secondary direction to waypoint movement

--- Assets/Scripts/entities/Mover.cs
+++ Assets/Scripts/entities/Mover.cs
@@ -34,15 +34,13 @@ public static class Mover
                 {
                     return false;
                 }
-                else
+                //If the character we interact with tells us that we shouldnt move, 
+                //we return false and we dont move
+                //if it doesnt, then we move
+                if (!hitCharacter.Interact(sourceCharacter))
                 {
-                    //If the character we interact with tells us that we shouldnt move, we return false and we dont move
-                    //if it doesnt, then we move
-                    if (!hitCharacter.Interact(sourceCharacter))
-                    {
-                        return false;
-                    }
-                }
+                    return false;
+                }                                                    
             }
             else
             {
@@ -59,31 +57,42 @@ public static class Mover
         return true;
     }
 
-    public static MovDir WaypointDirection(Character character, Vector3 waypoint)
+    public static void WaypointDirection(
+        Character character, 
+        Vector3 waypoint,
+        out MovDir primaryDirection,
+        out MovDir secondaryDirection)
     {
         Vector3 vector = waypoint - character.transform.position;
         bool forwardLeft = vector.z > vector.x;
         bool forwardRight = vector.z + vector.x > 0;
+        bool forward = vector.z > 0;
+        bool right = vector.x > 0;        
+
         if (forwardLeft)
         {
             if (forwardRight)
             {
-                return MovDir.FORWARD;
+                primaryDirection = MovDir.FORWARD;
+                secondaryDirection = right ? MovDir.RIGHT : MovDir.LEFT;
             }
-            else 
+            else // backLeft
             {
-                return MovDir.LEFT;
+                primaryDirection = MovDir.LEFT;
+                secondaryDirection = forward ? MovDir.FORWARD : MovDir.BACK;
             }
         }
-        else
+        else // backRight
         {
             if (forwardRight)
             {
-                return MovDir.RIGHT;
+                primaryDirection = MovDir.RIGHT;
+                secondaryDirection = forward ? MovDir.FORWARD : MovDir.BACK;
             }
-            else
+            else // backLeft
             {
-                return MovDir.BACK;
+                primaryDirection = MovDir.BACK;
+                secondaryDirection = right ? MovDir.RIGHT : MovDir.LEFT;
             }
         }
     }
