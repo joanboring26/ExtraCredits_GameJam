70e59942ed58102c361f1c661b9ab801235ddb39
Jake Smith <jakeconlonsmith@gmail.com>
2020-08-21 20:09:42 -0500

    Put together some more UI elements, recorded some voice lines

---
+++ Assets/Scripts/UIScripts/healthbarScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthbarScript : MonoBehaviour
+{
+    [SerializeField] GameObject healthbar;
+    float healthValue;
+    void Awake()
+    {
+        healthValue = healthbar.transform.localScale.x;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeHealth(float amount)
+    {
+        healthValue += amount;
+        healthbar.transform.localScale = new Vector3(healthValue, 1, 1);
+        if(healthValue < 0)
+            healthValue = 0;
+        else if(healthValue > 1)
+            healthValue = 1;
+        healthbar.transform.localScale = new Vector3(healthValue, 1, 1);
+    }
+}
---
+++ Assets/Scripts/testingToolsScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingToolsScript : MonoBehaviour
+{
+
+    [SerializeField] KeyCode increaseHealthbar;
+    [SerializeField] KeyCode decreaseHealthbar;
+    [SerializeField] KeyCode speedUpTime;
+    [SerializeField] float timeMultiplier;
+    [SerializeField] healthbarScript healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(increaseHealthbar))
+            healthbar.ChangeHealth(0.1f);
+        if(Input.GetKeyDown(decreaseHealthbar))
+            healthbar.ChangeHealth(-0.1f);
+        if(Input.GetKeyDown(speedUpTime))
+            if(Time.timeScale == 1)
+                Time.timeScale = timeMultiplier;
+            else
+                Time.timeScale = 1;
+    }
+}
