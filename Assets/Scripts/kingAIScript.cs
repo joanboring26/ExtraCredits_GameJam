ad5023b8390ea0d518dbd4cdd69a55aa28bc61a8
Jake Smith <jakeconlonsmith@gmail.com>
2020-08-22 13:03:09 -0500

    boop

---
+++ Assets/Scripts/kingAIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kingAIScript : Character
+{
+    private CharacterType thisType = CharacterType.KING;
+
+    [SerializeField] Vector2 destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void CharacterUpdate()
+    {
+        Character hitCharacter = new Character();
+        Mover.MoveCharacter(this, out hitCharacter, true, MovDir.FORWARD, Physics.DefaultRaycastLayers);
+    }
+}
