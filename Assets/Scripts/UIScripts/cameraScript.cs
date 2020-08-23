5433b45097b5f0ef23622efb3da52f4ec3cf0d4f
Jake Smith <jakeconlonsmith@gmail.com>
2020-08-23 15:08:29 -0500

    camera controls

--- Assets/Scripts/UIScripts/cameraScript.cs
+++ Assets/Scripts/UIScripts/cameraScript.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class cameraScript : MonoBehaviour
 {
     [SerializeField] GameObject player = null;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(0,15,-10);
+    private float sensitivity = 10.0f;
+    private Camera thisCam;
+    private float rotationX;
+    private float rotationY;
+    void Awake()
+    {
+        thisCam = transform.GetChild(0).gameObject.GetComponent<Camera>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         transform.SetParent(player.transform);
-        Vector3 position = player.transform.position;
-        position += offset;
-        transform.position = position;
+        transform.position = player.transform.position;
+        thisCam.transform.localPosition = offset;
+        Cursor.lockState = CursorLockMode.Locked;
+        //transform.localPosition += offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotationX += Input.GetAxis("Mouse Y") * -sensitivity;
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+        rotationX = Mathf.Clamp(rotationX, -50, 25);
+    }
+
+    void FixedUpdate()
+    {
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
     }
 
 
